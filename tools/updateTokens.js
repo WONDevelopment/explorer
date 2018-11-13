283f46e73e672f99d22e9e40ab9b4642a458a3b1
guoyunzhi <guoyunzhi@topgamesinc.com>
2018-11-13 15:56:36 +0800

    UpdateToken: fixed error without connect.

--- tools/updateTokens.js
+++ tools/updateTokens.js
@@ -2,27 +2,33 @@
 var fs = require('fs');
 const request = require('request');
 
-var tokensParam = []; // require('../public/tokens');
+const defaultTokens = require('../public/tokens');
+var tokensParam = [];
+
+var updateParam = function (tokens) {
+    var tokenAddrs = [];
+    for (var index in tokens)
+        tokenAddrs[index] = tokens[index].address;
+    tokensParam[0] = tokenAddrs;
+    tokensParam[1] = tokens;
+};
+updateParam(defaultTokens);
 
 var updateTokens = function () {
     request('https://raw.githubusercontent.com/WONDevelopment/explorer/master/public/tokens.json',
     // request('https://raw.githubusercontent.com/ethereumproject/explorer/master/public/tokens.json',
-        { json: true }, (err, res, body) => {
-        if (res.statusCode !== 200) {
-            console.log('UpdateTokens: ', body);
-            if (tokensParam.length) return;
-
-            body = require('../public/tokens');
-        }
-        // console.info(res);
-        var tokenAddrs = [];
-        for (var index in body)
-            tokenAddrs[index] = body[index].address;
-        tokensParam[0] = tokenAddrs;
-        tokensParam[1] = body;
-        fs.writeFile('./public/tokens.json', JSON.stringify(body, null, 2), 'utf8', function(err) {
+        { json: true/*, timeout: 15000 */}, (err, res, body) => {
+        if (res && res.statusCode === 200) {
+            updateParam(body);
+            fs.writeFile('./public/tokens.json', JSON.stringify(body, null, 2), 'utf8', function(err) {
                 if (err) console.error(err);
             });
+        } else {
+            if (res)
+                console.log('UpdateTokens: ', res.statusCode, res.statusMessage);
+            else
+                console.error(err);
+        }
     });
 
 };
